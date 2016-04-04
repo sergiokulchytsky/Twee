37fc81337bb5cce39d3cb2a60fdc10919bf07029
Darmaru <serhiyx16@gmail.com>
2016-04-05 01:39:11 +0300

    Start to implementation Navigation View

--- app/src/main/java/com/grreen/twee/MainActivity.java
+++ app/src/main/java/com/grreen/twee/MainActivity.java
@@ -2,15 +2,17 @@ package com.grreen.twee;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -19,7 +21,9 @@ public class MainActivity extends Activity {
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
+        if (toolbar != null) {
+            toolbar.setTitle(R.string.app_name);
+        }
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
